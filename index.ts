dd9f8bc5f00d56d7bdc3e1b043eface654000651
amaury <grillotamaury@gmail.com>
2022-04-07 16:29:20 +0200

    Remove cors

--- index.ts
+++ index.ts
@@ -4,15 +4,10 @@ import {Express} from "express";
 const express = require('express')
 
 const fileUpload = require('express-fileupload');
-const cors = require('cors');
+
 const app: Express = express();
 
-const corsOptions = {
-    origin: ['http://localhost:4200', 'https://oome-front.herokuapp.com'],
-    optionsSuccessStatus: 200, // For legacy browser support
-    methods: "GET, PUT, POST, DELETE"
-}
-app.use(cors(corsOptions));
+
 app.use(fileUpload());
 app.use(bodyParser.json());
 buildRoutes(app);
