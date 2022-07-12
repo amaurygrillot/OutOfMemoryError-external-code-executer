df31157b0f8fc5a1b7ddc67a9870dfeafc3cc529
amaurygrillot <39194528+amaurygrillot@users.noreply.github.com>
2022-07-12 20:23:22 +0200

    test isolate python

--- python-executer/python-executer.route.ts
+++ python-executer/python-executer.route.ts
@@ -1,4 +1,5 @@
 import {PythonExecuterController} from "./python-executer.controller";
+import ivm from "isolated-vm";
 
 
 const express = require('express')
@@ -6,20 +7,46 @@ export const pythonRouter = express.Router();
 
 pythonRouter.post("/", async function(req, res) {
     let file = req.files.fileKey;
-    console.log("filename : " + file.name);
-    const fs = require('fs');
-    const pythonExecuterController = new PythonExecuterController();
-    try {
-        fs.writeFile(`/app/mnt/storedPrograms/python/${file.name}`,file.data, function (err: any) {
-            if (err) return console.log(err);
-        });
-        const message = await pythonExecuterController.executeNoArgumentScript(file.name);
-        res.status(200).json(message).end();
-    }
-    catch (err) {
-        console.error(err);
-        res.status(500).json("erreur").end();
-    }
+    const ivm = require('isolated-vm');
+    const isolate = new ivm.Isolate({ memoryLimit: 128 });
+
+// Create a new context within this isolate. Each context has its own copy of all the builtin
+// Objects. So for instance if one context does Object.prototype.foo = 1 this would not affect any
+// other contexts.
+    const context = isolate.createContextSync();
+
+// Get a Reference{} to the global object within the context.
+    const jail = context.global;
+
+// This makes the global object available in the context as `global`. We use `derefInto()` here
+// because otherwise `global` would actually be a Reference{} object in the new isolate.
+    jail.setSync('global', jail.derefInto());
+
+// We will create a basic `log` function for the new isolate to use.
+    jail.setSync('log', function(...args) {
+        console.log(...args);
+    });
+
+// And let's test it out:
+    context.evalSync('log("hello world")');
+// > hello world
+
+// Let's see what happens when we try to blow the isolate's memory
+    const hostile = isolate.compileScriptSync(`
+	let test = "my isolated code did run";
+`);
+
+// Using the async version of `run` so that calls to `log` will get to the main node isolate
+    let result = hostile.run(context)
+        .then(res =>
+    {
+        res.status(200).json(result).end();
+    }).catch(err => {
+        res.status(500).json("erreur : " + err).end();
+        return;
+    });
+
+
 
 });
 
