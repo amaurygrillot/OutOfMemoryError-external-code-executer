e219302f0743f5166f6b840adce45dee2bab9d22
amaurygrillot <39194528+amaurygrillot@users.noreply.github.com>
2022-10-25 19:16:38 +0200

    Update execution_time.sh

--- execution_time.sh
+++ execution_time.sh
@@ -3,5 +3,4 @@ ts=$(date +%s%N);
 $@;
 tt=$(($(date +%s%N) - $ts));
 echo "Temps d'exécution : "
-echo "scale  = 6; $tt / 1000000000" | bc -l;
-echo " secondes"
+echo "scale  = 6; $tt / 1000000000; quit; secondes" | bc -l;
