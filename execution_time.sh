7079c9f859487d1c74a05478b41622ab222a3da3
amaurygrillot <39194528+amaurygrillot@users.noreply.github.com>
2022-10-25 19:35:05 +0200

    Update execution_time.sh

--- execution_time.sh
+++ execution_time.sh
@@ -2,4 +2,4 @@
 ts=$(date +%s%N);
 $@;
 tt=$(($(date +%s%N) - $ts));
-printf "Temps exécution : %.15f secondes" "$(echo "scale=15; $tt / 1000000" | bc -l)";
+printf "Temps exécution : %.15f secondes" "$(echo "scale=15; $tt / 1000000000" | bc -l)";
