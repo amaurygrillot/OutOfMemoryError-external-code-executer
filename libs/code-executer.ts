92740603325ec36b021fb7fa4276bb7d8e9d382f
amaurygrillot <39194528+amaurygrillot@users.noreply.github.com>
2022-10-25 14:05:40 +0200

    add time in command execution

--- libs/code-executer.ts
+++ libs/code-executer.ts
@@ -6,7 +6,7 @@ export function executeCommand(command: string, options: string[] | undefined, o
     let commandToExecute = command;
     if(process.env.CONTEXT === undefined || process.env.CONTEXT !== "local")
     {
-        fakechrootOptions = ['fakeroot', 'chroot', '/bullseye'];
+        fakechrootOptions = ['fakeroot', 'chroot', '/bullseye', 'time'];
         fakechrootOptions.push(command);
         commandToExecute = 'fakechroot';
     }
