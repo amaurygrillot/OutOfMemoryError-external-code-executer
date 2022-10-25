4f2d8f973958c8444a08ae0fc4d18bffaf62e413
amaurygrillot <39194528+amaurygrillot@users.noreply.github.com>
2022-10-25 19:25:38 +0200

    Update execution_time.sh

--- execution_time.sh
+++ execution_time.sh
@@ -2,5 +2,4 @@
 ts=$(date +%s%N);
 $@;
 tt=$(($(date +%s%N) - $ts));
-echo "Temps d'exécution : "
-echo "scale  = 6; $tt / 1000000 millisecondes" | bc -l;
+printf 'Temps exécution : %.3f secondes' "$(echo "scale=3; $tt / 1000000" | bc -l)";
